1bd44fb221e0caa866e598ebaf0aaf1c2bc8f014
“hareesh600” <harypeace@gmail.com>
2024-02-29 16:29:25 +0530

    first commit

---
+++ Front_end/medium-clone/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Banner from './components/Banner'
+function App() {
+
+  
+
+  return (
+    <>
+    <Header></Header>
+    <Banner></Banner>
+     <p>hello</p>
+    </>
+  )
+}
+
+export default App
